686743a8c6900b267301626836a7a18a379a5624
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2021-03-17 15:29:25 +0800

    note_list

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -7,11 +7,39 @@ class NoteList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Notes'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Hello World'),
-        ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button Clicked!');
+        },
+        backgroundColor: Colors.deepOrange,
+        tooltip: 'Add Note',
+        child: Icon(Icons.add),
       ),
+    );
+  }
+
+  ListView getNoteListView() {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            title: Text(
+              'My List Tile',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            subtitle: Text('Letak tarikh note'),
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Icon(Icons.keyboard_arrow_right),
+            ),
+            trailing: Icon(Icons.delete, color: Colors.grey),
+          ),
+        );
+      },
     );
   }
 }
