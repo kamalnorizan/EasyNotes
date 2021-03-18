d3fdb090bfc8e1efa219e9073e2cf74e4ca4a512
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2021-03-18 10:37:56 +0800

    note model p2 (getters & setters)

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -9,4 +9,32 @@ class Note {
 
   Note.withId(this._id, this._title, this._date, this._prority,
       [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get prority => _prority;
+
+  set title(String newtitle) {
+    if (newtitle.length <= 255) {
+      this._title = newtitle;
+    }
+  }
+
+  set description(String newdescription) {
+    if (newdescription.length <= 255) {
+      this._description = newdescription;
+    }
+  }
+
+  set date(String newdate) {
+    this._date = newdate;
+  }
+
+  set prority(int newprority) {
+    if (newprority >= 1 && newprority <= 2) {
+      this._prority = newprority;
+    }
+  }
 }
